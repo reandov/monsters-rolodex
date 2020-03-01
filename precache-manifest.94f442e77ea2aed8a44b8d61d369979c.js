b065945da6970fdea191354bfb6d9479b02a5c41
Evandro Rodrigues <evandro.rodriguescc@gmail.com>
2020-03-01 15:46:40 -0300

    Updates

---
+++ precache-manifest.94f442e77ea2aed8a44b8d61d369979c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a924d965729c492369db7cce26ddccb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b599e4bedb1874738b5c",
+    "url": "/monsters-rolodex/static/css/main.2c4b88d0.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b599e4bedb1874738b5c",
+    "url": "/monsters-rolodex/static/js/main.c944ccaf.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
